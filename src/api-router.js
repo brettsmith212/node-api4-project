2f971b09c72a8dcff59d28caeb3ff62736a9a214
Brett Smith <brettsmith212@gmail.com>
2022-02-10 11:15:49 -1000

    register

--- src/api-router.js
+++ src/api-router.js
@@ -18,7 +18,23 @@ const usersData = [
 ];
 
 router.get("/users", (req, res) => {
-  res.status(200).json(usersData);
+  res.status(200).json(
+    usersData.map((user) => {
+      return user.username;
+    })
+  );
+});
+
+router.post("/register", (req, res) => {
+  let { username, password } = req.body;
+  if (!username || !password) {
+    res.status(400).json({ message: "missing username and password" });
+  } else {
+    res.status(201).json({
+      username: username,
+      password: password,
+    });
+  }
 });
 
 module.exports = router;
